579b4e4ba36fbaa81940ac16051038d43415836a
unknown <ganchin94@gmail.com>
2025-10-28 00:20:20 +0200

    little opssI

---
+++ Loops-Data-Type/taskEleven.js
@@ -0,0 +1,12 @@
+let xA = 10;
+let yA = -3;
+
+  if (yA > 0 && xA > 0){
+    console.log(`First Quadrant`);
+  }else if(yA > 0 && xA < 0){
+    console.log(`Second Quadrant`);
+  }else if(yA < 0 && xA < 0){
+    console.log(`Third Quadrant`);
+  }else if(yA < 0 && xA > 0){
+    console.log(`Fourth Quadrant`);
+  }
---
+++ Loops-Data-Type/taskTwelve.js
@@ -0,0 +1,9 @@
+let sideA = 0;
+let sideB = 0;
+let sideC = 0;
+
+if (sideA < sideB + sideC && sideB < sideA + sideC && sideC < sideB + sideA){
+  console.log(`yes`);
+}else { 
+  console.log(`No..`);
+}
